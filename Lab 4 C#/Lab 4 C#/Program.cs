bd5fbfb4970f11d0f4d2c549e72942185c4b5272
SkorpikFeed <125889197+SkorpikFeed@users.noreply.github.com>
2023-04-04 21:57:47 +0300

    Add files via upload

---
+++ Lab 4 C#/Lab 4 C#/Program.cs
@@ -0,0 +1,61 @@
+﻿using  Lab_4_C_;
+
+class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Enter name of the student");
+        string name = System.Console.ReadLine();
+        System.Console.WriteLine("Enter surname of the student");
+        string surname = System.Console.ReadLine();
+        System.Console.WriteLine("Enter middle name of the student");
+        string middle = System.Console.ReadLine();
+        Students students = new Students(name, surname, middle);
+
+        for(int i = 0; i > -1; i++)
+        {
+            System.Console.ForegroundColor = System.ConsoleColor.Green;
+            System.Console.WriteLine("If you want to change information about the student enter 1");
+            System.Console.WriteLine("If you want to add new student enter 2");
+            System.Console.WriteLine("If you want to STOP enter 3");
+            System.Console.WriteLine("If you want to get table of the students enter 4");
+            System.Console.ForegroundColor = System.ConsoleColor.White;
+            string symb = System.Console.ReadLine();
+            switch(symb)
+            {
+                case "1":
+                    System.Console.WriteLine("Enter number of the student");
+                    int number = int.Parse(System.Console.ReadLine());
+                    System.Console.WriteLine("Enter name of the student");
+                    string name1 = System.Console.ReadLine();
+                    System.Console.WriteLine("Enter surname of the student");
+                    string surname1 = System.Console.ReadLine();
+                    System.Console.WriteLine("Enter middle name of the student");
+                    string middle1 = System.Console.ReadLine();
+                    students[number] = name1 + " " + surname1 + " " + middle1;
+                    break;
+                case "2":
+                    System.Console.WriteLine("Enter name of the student");
+                    string name2 = System.Console.ReadLine();
+                    System.Console.WriteLine("Enter surname of the student");
+                    string surname2 = System.Console.ReadLine();
+                    System.Console.WriteLine("Enter middle name of the student");
+                    string middle2 = System.Console.ReadLine();
+                    students[students.Count] = name2 + " " + surname2 + " " + middle2;
+                    break;
+                case "3":
+                    i = -2;
+                    break;
+                case "4":
+                    for(int j = 0; j < students.Count; j++)
+                    {
+                        System.Console.WriteLine(students[j]);
+                    }
+                    break;
+                default:
+                    System.Console.WriteLine("You entered wrong number");
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab 4 C#/Lab 4 C#/Students.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_C_
+{
+    internal class Students
+    {
+        private List<string> names = new List<string>();
+        private List<string> surnames = new List<string>();
+        private List<string> middleNames = new List<string>();
+        private int count = 0;
+        public Students() 
+        {
+        }
+        public Students(string name,string surname,string middle) 
+        {
+            names.Add(name);
+            surnames.Add(surname);
+            middleNames.Add(middle);
+            count++;
+        }
+        public string this[int index]
+        {
+            set 
+            {
+                string[] parts = value.Split(' ');
+                if(index < 0 || index > count)
+                {
+                    System.Console.WriteLine("You entered wrong index");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                else if(index == count) 
+                {
+                    names.Add(parts[0]);
+                    surnames.Add(parts[1]);
+                    middleNames.Add(parts[2]);
+                    count++;
+                }
+                else
+                {
+                    names[index] = parts[0];
+                    surnames[index] = parts[1];
+                    middleNames[index] = parts[2];
+                }
+            }
+            get 
+            {
+                if(index < 0 || index > count)
+                {
+                    System.Console.WriteLine("You entered wrong index");
+                    return null;
+                }
+                else
+                {
+                    return names[index] + " " + surnames[index] + " " + middleNames[index];
+                }
+            }
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+    }
+}
